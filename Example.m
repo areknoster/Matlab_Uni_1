42df768250d82393e50f713a1a1844a15d026d65
noster arkadiusz <nostera@p21709w.MINI>
2019-10-16 15:02:56 +0200

    fixes at lab

--- Example.m
+++ Example.m
@@ -1,9 +1,10 @@
 numberOfSamples = 40;
-firstMatrixSize = 10;
+firstMatrixSize = 20;
 increment = 20;
 
 data_v1 = generateData(numberOfSamples, firstMatrixSize, increment, @GEPPv1);
 data_v2 = generateData(numberOfSamples, firstMatrixSize, increment, @GEPPv2);
 
 LinearyDependent = generateRandomExample(10, true);
+%Expected to throw an error
 GEPPv2(LinearyDependent);
--- Generators/generateData.m
+++ Generators/generateData.m
@@ -12,14 +12,15 @@ function data = generateData(N, size, increment, GEPP_function)
 %       x
 %   data is cellural table with "matrix_size", "matrix", "result",
 %   "elapsed_time", "result_error_vector", "max_error" respectively.
-data = cell(N+1, 6);
-data(1, :) = {"matrix_size", "matrix", "result", "elapsed_time", "result_error_vector", "max_error"};
+data = cell(N+1, 7);
+data(1, :) = {"matrix_size", "matrix", "result", "elapsed_time", "elapsed_time/N^3", "result_error_vector", "max_error"}; %#ok<CLARRSTR>
 for i = 2:N+1
     data{i, 1} = size .* 2; % Matrix size
     data{i, 2} = generateRandomExample(size); %matrix
     [data{i, 3}, data{i, 4}] = GEPP_function(data{i, 2}); %result and elapsed time
-    data{i, 5} = solution_check(data{i, 2}, data{i, 3}); %actual result error
-    data{i,6} = max(abs(data{i, 5})); % maximal error
+    data{i, 5} = data{i, 4} / (data{i, 1} .^ 3); % elapsed_time/N^3
+    data{i, 6} = solution_check(data{i, 2}, data{i, 3}); %actual result error
+    data{i,7} = max(abs(data{i, 5})); % maximal error
     size = size + increment;
 end
 end
